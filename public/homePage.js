99b43950567a5e742f2d446c08cfcd641b880455
gashpen <constantinent@mail.ru>
2023-03-05 13:43:05 +0300

    work_done_v2.0?

--- public/homePage.js
+++ public/homePage.js
@@ -1,11 +1,10 @@
 "use strict"
 const logoutButton = new LogoutButton();
-logoutButton.action = () =>{
-    const callback = (response) =>{
-        if(response.success){
-            location.reload();
-        }
+logoutButton.action = (callback = (response) =>{
+    if(response.success){
+        location.reload();
     }
+}) =>{
     ApiConnector.logout(callback);
 }
     ApiConnector.current((response)=>{
@@ -25,43 +24,43 @@ function exchangeRate(){
     })
     
 }
-
+exchangeRate()
 setInterval(()=>{
     exchangeRate()
 },60000);
 
 const moneyManager = new MoneyManager();
 
-moneyManager.addMoneyCallback = (data) =>{
-    const {currency, amount} = data;
-    const callback = (response) =>{
-        if(response.success){
-            ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage();
-        }
+moneyManager.addMoneyCallback = ({currency, amount} = data, callback = (response) =>{
+    if(response.success){
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success, "Счет успешно пополнен!");
+    } else {
+        moneyManager.setMessage(response.success, "Не удалось пополнить счет!")
     }
+}) =>{
     ApiConnector.addMoney({ currency, amount }, callback);
 }
 
-moneyManager.conversionMoneyCallback = (data)=>{
-    const {fromCurrency, targetCurrency, fromAmount} = data;
-    const callback = (response) =>{
-        if(response.success){
-            ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage();
-        }
+moneyManager.conversionMoneyCallback = ({fromCurrency, targetCurrency, fromAmount} = data, callback = (response) =>{
+    if(response.success){
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success,"Средства успешно конвертированны!");
+    } else {
+        moneyManager.setMessage(response.success,"Не удалось конвертировать средства!");
     }
+})=>{
     ApiConnector.convertMoney({ fromCurrency, targetCurrency, fromAmount }, callback);
 }
 
-moneyManager.sendMoneyCallback = (data) =>{
-    const {to, currency, amount} = data;
-    const callback = (response) =>{
-        if(response.success){
-            ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage();
-        }
-  }
+moneyManager.sendMoneyCallback = ({to, currency, amount} = data,callback = (response) =>{
+    if(response.success){
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success, "Средства успешно переведены пользователю!");
+    } else {
+        moneyManager.setMessage(response.success, "Пользователя не существует или недостаточно средств")
+    }
+}) =>{
   ApiConnector.transferMoney({ to, currency, amount }, callback);
 }
 
@@ -77,28 +76,24 @@ const callback = (response) =>{
 
 ApiConnector.getFavorites(callback);
 
-favoritesWidget.addUserCallback = (data)=>{
-    const {id, name} = data;
-    const callback = (response) =>{
-        if(response.success){
-            favoritesWidget.clearTable();
-            favoritesWidget.fillTable(response.data);
-            moneyManager.updateUsersList(response.data);
-            moneyManager.setMessage();            
-        }
-}
+favoritesWidget.addUserCallback = ({id, name} = data,callback = (response) =>{
+    if(response.success){
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+        moneyManager.setMessage(response.success, "Пользователь успешно добавлен!");            
+    }
+})=>{
 ApiConnector.addUserToFavorites({id, name}, callback);
 }
 
-favoritesWidget.removeUserCallback = (data)=>{
-    const id = data;
-    const callback = (response) =>{
-        if(response.success){
-            favoritesWidget.clearTable();
-            favoritesWidget.fillTable(response.data);
-            moneyManager.updateUsersList(response.data);
-            moneyManager.setMessage();          
-        }
-}
+favoritesWidget.removeUserCallback = (id = data,callback = (response) =>{
+    if(response.success){
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+        favoritesWidget.setMessage(response.success, "Пользователь удален!");          
+    }
+})=>{
 ApiConnector.removeUserFromFavorites(id, callback);
 }
